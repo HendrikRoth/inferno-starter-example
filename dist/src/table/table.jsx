f174361755debb2fb4c5424e7418fa8e846f8379
Hendrik Roth <hi@hendrikroth.com>
2016-08-04 22:03:05 +0200

    initial commit

---
+++ dist/src/table/table.jsx
@@ -0,0 +1,21 @@
+"use strict";
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var inferno_component_1 = require('inferno-component');
+var TableComponent = (function (_super) {
+    __extends(TableComponent, _super);
+    function TableComponent(props) {
+        _super.call(this, props);
+    }
+    TableComponent.prototype.render = function () {
+        return (<table></table>);
+    };
+    return TableComponent;
+}(inferno_component_1.default));
+;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = TableComponent;
+//# sourceMappingURL=table.jsx.map
---
+++ src/table/table.tsx
@@ -0,0 +1,14 @@
+import Component from 'inferno-component'
+
+class TableComponent extends Component {
+	constructor(props: any) {
+		super(props)
+	}
+	render() {
+		return (
+			<table></table>
+		)
+	}
+};
+
+export default TableComponent;
